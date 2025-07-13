d423fe360937572be44ea2c6285df8728dedf44d
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-13 14:13:40 +0200

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+const github = document.querySelector(".fa-square-github");
+const myGithubURL = "https://github.com/ShravanRamjathan";
+github.addEventListener("click", () => {
+  window.open(myGithubURL, "_blank");
+});
